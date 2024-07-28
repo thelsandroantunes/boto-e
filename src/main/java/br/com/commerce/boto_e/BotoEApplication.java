a8f827719aa26bbffdc707c086380bc9e2b90355
thelsandro.developer <thelsandro.developer@gmail.com>
2024-07-28 09:30:22 -0400

    project ms user-management

---
+++ src/main/java/br/com/commerce/boto_e/BotoEApplication.java
@@ -0,0 +1,13 @@
+package br.com.commerce.boto_e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotoEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotoEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/commerce/boto_e/BotoEApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.commerce.boto_e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotoEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
